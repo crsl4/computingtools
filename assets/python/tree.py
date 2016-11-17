316702ac9924444568ed10a4a111128ec3d92b1a
Cecile Ane <cecile.ane@wisc.edu>
2016-11-17 16:36:27 -0600

    new python module "tree", done in class

--- assets/python/tree.py
+++ assets/python/tree.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 
-a = 5
-class Foo:
-    def __init__(self):
-        self.x = a
-class Bar:
-    a = 6 # will be object attribute: .a
-    b = ["u","v"]
-    def __init__(self):
-        self.x = a
-
 class Edge:
     """Edge class, to contain a directed edge of a tree or directed graph.
     attributes parent and child: index of parent and child node in the graph.
@@ -18,15 +8,12 @@ class Edge:
     def __init__ (self, parent, child, length=None):
         """create a new Edge object, linking nodes
         with indices parent and child."""
-        print("starting __init__ for new Edge object/instance")
         self.parent = parent
         self.child = child
         self.length = length
 
     def __str__(self):
         res = "edge from " + str(self.parent) + " to " + str(self.child)
-        if self.length:
-            res += ", length=" + str(self.length)
         return res
 
 class Tree:
@@ -34,7 +21,8 @@ class Tree:
     def __init__(self, edgelist):
         """create a new Tree object from a list of existing Edges"""
         self.edge = edgelist
-        self.update_node2edge() # creates attributes node2edge and root
+        if edgelist:
+            self.update_node2edge()
 
     def __str__(self):
         res = "parent -> child:"
@@ -63,45 +51,34 @@ class Tree:
             raise Exception("there should be at least one root!")
         self.root = rootset.pop()
 
-    def get_dist2root(self, i):
-        """distance of node index i to the root"""
-        # check if e is root: if so: distance 0
-        if i==self.root:
-            return 0
-        else:
-            e = self.node2edge[i]
-            return 1 + self.get_dist2root(e.parent)
-
     def get_path2root(self, i):
-        """list of nodes from the root to i (in this order)"""
-        if i==self.root:
-            return [i]
-        else:
-            e = self.node2edge[i]
-            res = self.get_path2root(e.parent)
-            res.append(i)
-            return res
-
-    def get_MRCA(self, i, j):
-        """return most recent common ancestor between nodes i and j"""
-        pathi = self.get_path2root(i)
-        pathj = self.get_path2root(j)
-        while pathi and pathj:
-            if pathi[0]==pathj[0]:
-                res = pathi.pop(0)
-                pathj.pop(0)
-            else:
+        """takes the index i of a node and returns the list of nodes
+        from i to the root, in this order.
+        This function is written with a loop.
+        An alternative optional would have been a recursive function:
+        that would call itself on the parent of i (unless i is the root)"""
+        res = []
+        nextnode = i
+        while True:
+            res.append(nextnode) # add node to the list
+            if nextnode == self.root:
                 break
+            nextnode = self.node2edge[nextnode].parent # grab the parent to get closer to root
         return res
 
     def get_nodedist(self, i, j):
-        """tree distance between node i and node j"""
-        pathi = self.get_path2root(i)
+        """takes 2 nodes and returns the distance between them:
+        number of edges from node i to node j"""
+        if i==j:
+            return 0
+        pathi = self.get_path2root(i) # last node in this list: root
         pathj = self.get_path2root(j)
         while pathi and pathj:
-            if pathi[0]==pathj[0]:
-                pathi.pop(0)
-                pathj.pop(0)
+            anci = pathi[-1] # anc=ancestor, last one
+            ancj = pathj[-1]
+            if anci == ancj:
+                pathi.pop()
+                pathj.pop()
             else:
                 break
         return len(pathi)+len(pathj)
